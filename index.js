5bd8ea69c0879d9b3ca55d8ee42551e80ddf1aaf
Nick Lediet <nick@tagsforhope.com>
2019-04-05 11:49:18 -0400

    Created script

---
+++ index.js
@@ -0,0 +1,72 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const bluetoothctl = require('bluetoothctl')
+
+const desiredDeviceName = 'MX Anywhere 2S'
+
+async function main() {
+    try{
+        const { stdout, stderr } = await exec('bt-device -l')
+
+        if(stderr) throw new Error(stderr)
+
+        const [,...foundDevices] = stdout.split('\n') // Remove 'Added devices:' label
+        
+        const devices = foundDevices
+            .filter(d => !!d)
+            .map(d => {
+                const matches = d.match(/\((.+)\)$/)
+                const name = d.replace(matches[0], '').replace(/\s$/, '')
+
+                const device = {
+                    name,
+                    address: matches[1]
+                }
+
+                return device
+            })
+
+        devices.forEach(async d => {
+            if(d.name !== desiredDeviceName) {
+                return;
+            }
+            try {
+                const {stdout, stderr} = await exec(`bt-device -r ${d.address}`) // Remove all duplicates
+    
+                if(!!stderr) throw new Error(stderr)
+    
+                console.log(stdout)
+            } catch(error) {
+                console.error(error)
+            }
+        })
+
+        bluetoothctl.Bluetooth()
+
+        bluetoothctl.on(bluetoothctl.bluetoothEvents.Device, async devices => {
+            const device = devices.filter(d => d.name === desiredDeviceName)
+
+            if(!!device.length) {
+                try {
+                    //Connect
+                    const { stdout, stderr } = await exec(`bt-device -c ${device[0].mac}`)
+
+                    if(!!stderr) throw new Error(stderr)
+
+                    bluetoothctl.scan(false)
+                
+                    process.exit()
+
+                } catch(error) {
+                    console.error(error)
+                }
+            }
+        })
+        
+        bluetoothctl.scan(true)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+main()
